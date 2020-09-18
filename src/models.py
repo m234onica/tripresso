81133feb42363924070d7f046166274ee08e2855
joycehsu <m234onica@gmail.com>
2020-09-18 11:57:27 +0800

    define dataclass to display json data

--- src/models.py
+++ src/models.py
@@ -1,9 +1,19 @@
 from sqlalchemy import Column, String, Date, DateTime, Integer, func
 from src.db import Base
+from dataclasses import dataclass
+import datetime
 
-
+@dataclass
 class TourGroups(Base):
     __tablename__ = "TourGroups"
+    id: int
+    name: str
+    tags: str
+    review: str
+    place: str
+    createdAt: datetime.datetime
+    deletedAt: datetime.datetime
+
     id = Column(Integer, primary_key=True, comment="旅團代號")
     name = Column(String(100), nullable=False, comment="旅團名")
     tags = Column(String(100), nullable=True, comment="旅團標籤的對照ID")
@@ -14,37 +24,39 @@ class TourGroups(Base):
                        server_default=func.now(), comment="建立時間")
     deletedAt = Column(DateTime, nullable=True, comment="刪除時間")
 
-    def as_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-
     def __repr__(self):
         return "<TourGroups: {}, {}, {}, {}>".format(self.id, self.name, self.createdAt, self.place)
 
 
+@dataclass
 class Tags(Base):
     __tablename__ = "Tags"
+    id: int
+    name: str
+    createdAt: datetime.datetime
+
     id = Column(Integer, primary_key=True, comment="標籤代號")
     name = Column(String(50), nullable=False, unique=True, comment="標籤名")
     createdAt = Column(DateTime, nullable=False,
                        server_default=func.now(), comment="建立時間")
 
-    def as_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-
     def __repr__(self):
         return "<Tags: {}, {}>".format(self.id, self.name)
 
 
+@dataclass
 class Price(Base):
     __tablename__ = "Price"
+    id: int
+    groupId: str
+    startDate: str
+    createdAt: datetime.datetime
+
     id = Column(Integer, primary_key=True, comment="價格代號")
     groupId = Column(Integer, nullable=False, comment="旅團代號")
     startDate = Column(Date, nullable=False, comment="出發日期")
     createdAt = Column(DateTime, nullable=False,
                        server_default=func.now(), comment="建立時間")
-
-    def as_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
     def __repr__(self):
         return "<Price: {}, {}>".format(self.id, self.groupId, self.startDate)
