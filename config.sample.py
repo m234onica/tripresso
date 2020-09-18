4bc5fb35f1dd1b8b667a4cd7f533e83641a432cd
joycehsu <m234onica@gmail.com>
2020-09-18 11:56:50 +0800

    fix: jsonify display zh-TW is garbled

--- config.sample.py
+++ config.sample.py
@@ -10,6 +10,7 @@ mysql = {
 
 class Config:
     DEBUG = True
+    JSON_AS_ASCII = False
 
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{NAME}?charset=utf8mb4".format(
         USER=mysql["username"],
